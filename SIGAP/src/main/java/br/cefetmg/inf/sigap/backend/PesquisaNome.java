29db5898c64c48cd95a24756da8393aad9885d32
egrfj <edjunior5002@gmail.com>
2024-12-17 10:48:21 -0300

    005 - Início de implementação

---
+++ SIGAP/src/main/java/br/cefetmg/inf/sigap/backend/PesquisaNome.java
@@ -0,0 +1,18 @@
+package br.cefetmg.inf.sigap.backend;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.*;
+import java.io.*;
+
+@WebServlet(name = "PesquisaNome", urlPatterns = {"/PesquisaNome"})
+
+public class PesquisaNome extends HttpServlet {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+            response.setContentType("text/html;charset=UTF-8");
+            try (PrintWriter out = response.getWriter()) {
+
+            }
+        }
+}
