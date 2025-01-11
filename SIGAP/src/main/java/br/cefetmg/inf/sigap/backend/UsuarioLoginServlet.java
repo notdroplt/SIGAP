988e39f04666abfe4cf6dbefdd99e605760cb9fa
Mysendell <klebinpc@gmail.com>
2025-01-11 10:09:28 -0300

    003 - Agora os servlets aceitam ambos formatos de cpf

--- SIGAP/src/main/java/br/cefetmg/inf/sigap/backend/UsuarioLoginServlet.java
+++ SIGAP/src/main/java/br/cefetmg/inf/sigap/backend/UsuarioLoginServlet.java
@@ -9,6 +9,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @WebServlet(urlPatterns = "/LoginServlet")
 public class UsuarioLoginServlet extends HttpServlet {
@@ -20,8 +22,16 @@ public class UsuarioLoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String senha = request.getParameter("senha");
-        long cpf = Long.parseLong(request.getParameter("cpf"));
 
+        StringBuilder cpfString = new StringBuilder();
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(request.getParameter("cpf"));
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+            cpfString.append(matcher.group());
+        }
+
+        long cpf = Long.parseLong(cpfString.toString());
         int token;
         if (UsuarioService.login(cpf, senha)) {
            token  = UsuarioService.getToken(cpf, senha);
